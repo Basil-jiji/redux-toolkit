4be299d853b2415c4ff67dc2ad1341ad10e03bed
Basil Jiji <basiljiji2015@gmail.com>
2023-07-14 11:00:22 +0530

    Cake Restocking

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 /*
   Action
@@ -21,7 +22,14 @@ const CAKE_ORDERED = 'CAKE_ORDERED'
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  }
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   }
 }
 
@@ -48,6 +56,11 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       }
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      }
     default:
       return state
   }
@@ -63,12 +76,15 @@ const reducer = (state = initialState, action) => {
 */
 
 const store = createStore(reducer)
-console.log('Initial state', store.getState());
+console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(()=>console.log('Updated State ',store.getState()))
+const unsubscribe = store.subscribe(() =>
+  console.log('Updated State ', store.getState())
+)
 
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+store.dispatch(restockCake(3))
 
 unsubscribe()
