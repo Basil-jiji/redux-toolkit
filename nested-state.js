9c5eff6dca7f5e3168312a3b75d357165ac4036e
Basil Jiji <basiljiji2015@gmail.com>
2023-07-14 12:25:58 +0530

    Nested State

---
+++ nested-state.js
@@ -0,0 +1,44 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+  name: 'Basil Jiji',
+  address: {
+    street: '123 Main St',
+    city: 'Kozhikkode',
+    state: 'kerala',
+  },
+}
+
+const STREET_UPDATED = 'STREET_UPDATED'
+
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,  //nested state
+          street: action.payload,
+        },
+      }
+    default: {
+      return state
+    }
+  }
+}
+
+const store = redux.createStore(reducer)
+console.log('InitialState ', store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state ', store.getState())
+})
+store.dispatch(updateStreet('456 Main St'))
+unsubscribe()
