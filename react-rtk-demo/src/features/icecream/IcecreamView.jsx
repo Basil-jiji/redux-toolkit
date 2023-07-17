0ac72bd78cbb86e1fd705cf39382f94ec201319b
Basil Jiji <basiljiji2015@gmail.com>
2023-07-17 14:52:19 +0530

    Dispatch an action by the user input value

--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ordered, restocked } from './icecreamSlice'
 
 export const IcecreamView = () => {
   const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
+  const [value, setValue] = useState(1)
   const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of Icecreams - {numOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order Icecream</button>
-      <button onClick={() => dispatch(restocked(3))}>Restock Icecreams</button>
+      <input type="number" value={value} onChange={e => setValue(parseInt(e.target.value))}/>
+      <button onClick={() => dispatch(restocked(value))}>Restock Icecreams</button>
     </div>
   )
 }
