6d6a884a18c8c75754831995bfca9a452b5473f4
Basil Jiji <basiljiji2015@gmail.com>
2023-07-17 13:02:36 +0530

    useSelector To Read data from the store

--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state)=>state.icecream.numOfIcecreams)
   return (
     <div>
-        <h2>Number of Icecreams - </h2>
+        <h2>Number of Icecreams - {numOfIcecreams}</h2>
         <button>Order Icecream</button>
         <button>Restock Icecreams</button>
     </div>
